467e9ada461222f20b5b526d0a799cb261d081af
caya8205-2 <yaandere200@gmail.com>
2026-05-12 15:39:34 +0700

    tes

--- apps/web-api/src/app.ts
+++ apps/web-api/src/app.ts
@@ -13,7 +13,7 @@ import { errorHandler } from "./middlewares/error.middleware";
 
 const app: Express = express();
 
-// Middlewares
+// Middlewares 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
